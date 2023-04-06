292c85b372e10e74ef50cf87136cc4340100157f
Tchaikovsky66 <21290050qq.com>
2023-04-06 20:53:13 +0800

    第一次修改

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `Faye blog`;
 export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_EMAIL = '212900750@qq.com'
+export const SITE_NAME = 'platos.phaedo.top';
+export const SITE_URL = "https://paltos.phaedo.top:82";
