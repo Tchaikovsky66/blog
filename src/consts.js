d0269e43eff7067fdab6c55d4501682a90a85ade
Tchaikovsky66 <212900750@qq.com>
2023-04-07 14:36:54 +0800

    title->phaedo

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Faye blog`;
+export const SITE_TITLE = `Phaedo's Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = '212900750@qq.com'
 export const SITE_NAME = 'platos.phaedo.top';
